51daeccccdfffebcc9a95ff189de2ce7cb64a409
upadhyayaditi <au.aditi08@gmail.com>
2024-03-08 21:04:58 +0530

    math.js file added

--- Hello-World/hello.js
+++ Hello-World/hello.js
@@ -1,1 +1,13 @@
+// Output a message to the console
 console.log('Hey there! I am JS');
+
+// Import the 'math' module using require
+const math = require("./math");
+
+// Output the result of adding 2 and 3 using the 'add' function from the 'math' module
+console.log("The sum is:", math.add(2,3));
+
+// Output the result of subtracting 6 from 7 using the 'sub' function from the 'math' module
+console.log("The sub is:", math.sub(6,7));
+
+// The code imports a module named 'math' and uses functions from that module to perform arithmetic operations.
