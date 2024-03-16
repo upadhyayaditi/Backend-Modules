2197372710dcc2d54e0e0c48421fde5be9b1adf1
upadhyayaditi <au.aditi08@gmail.com>
2024-03-16 22:25:48 +0530

    Server config done

--- MegaProject/src/index.js
+++ MegaProject/src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongo DB connection failed!!, error");
+})
